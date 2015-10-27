13a87a9b2aee67abdedb942614df2df7f2f41225
Antonin Delpeuch <antonin@delpeuch.eu>
2015-10-27 13:49:45 +0100

    Attempt to break build

--- www/association/compile.sh
+++ www/association/compile.sh
@@ -2,6 +2,7 @@
 
 mkdir -p _build
 
+exit 1
 for lang in fr en; do
     echo "Compiling in $lang"
     python -m markdown  -x markdown.extensions.toc index.$lang.md | cat html/$lang/head - html/$lang/foot > _build/index.$lang.html

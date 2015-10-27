4b813797eb384c1a39819208f042de12b6b6885a
Antoine Amarilli <a3nm@a3nm.net>
2015-10-27 17:47:57 +0100

    set -e for compile.sh

--- www/association/compile.sh
+++ www/association/compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "$DIR"
 
@@ -8,11 +10,8 @@ for lang in fr en; do
     OUT="_build/index.html.${lang}"
     HEAD="html/$lang/head"
     FOOT="html/$lang/foot"
-    ../compile_one "$IN" "$OUT" "$HEAD" "$FOOT" || {
-      echo "error"
-      # TODO: do this when we are sure about how the hook behaves
-      # exit 1;
-    }
+    # if this fails, set -e ensure that we will fail
+    ../compile_one "$IN" "$OUT" "$HEAD" "$FOOT"
 done
 
 
--- www/dev/compile.sh
+++ www/dev/compile.sh
@@ -3,6 +3,8 @@
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 cd "$DIR"
 
+set -e
+
 ../compile_one index.md _build/index.html html/head html/foot
 cp -r img _build/
 
